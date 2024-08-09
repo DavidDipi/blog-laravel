b466edcc64a534e46ad104a4c7da26ccf388c69b
David <daviddipi27@gmail.com>
2024-08-08 22:03:38 -0500

    [ factories ]

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,9 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Factory for Post model
+        Post::factory(100)->create();
+
         $this->call([
             UserSeed::class,
-            PostSeed::class,
         ]);
+
+        User::factory(10)->create();
     }
 }
